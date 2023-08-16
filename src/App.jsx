a8db9930eff9a28c0f8ca22855796d7a188616bc
alefo28 <alefo28@gmail.com>
2023-08-16 12:28:28 -0600

    change app

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Test Actions en Github</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          contar es{count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
