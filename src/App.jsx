135babcbb55458d30c051797c215ac114f48f181
alefo28 <alefo28@gmail.com>
2023-08-17 12:55:42 -0600

    change app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Test Actions en Github Cambios</h1>
+      <h1>Test Actions !!!!!!!!!!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           contar es{count}
